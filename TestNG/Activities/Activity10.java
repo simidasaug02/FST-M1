5e92c26a9e3d13a79ecb4cdd4e422fea5a26ffcb
simidasaug02 <141907857+simidasaug02@users.noreply.github.com>
2023-09-12 21:23:01 +0530

    Add files via upload

--- TestNG/Activities/Activity10.java
+++ TestNG/Activities/Activity10.java
@@ -15,6 +15,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -24,10 +26,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Activity10 {
-    private WebDriver driver;
-    private WebDriverWait wait;
+     WebDriver driver;
+     WebDriverWait wait;
 
-    @BeforeClass
+    @BeforeMethod
     public void setup() {
         WebDriverManager.firefoxdriver().setup();
         System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "NULL");
@@ -41,24 +43,18 @@ public class Activity10 {
     public static List<List<Object>> readExcel(String filePath) {
         List<List<Object>> data = new ArrayList<>();
         try {
-            // Read the file as a stream
+
             FileInputStream file = new FileInputStream(filePath);
-
-            // Create the workbook
             XSSFWorkbook workbook = new XSSFWorkbook(file);
-
-            // Get first sheet from that workbook
             XSSFSheet sheet = workbook.getSheetAt(0);
 
-            // Iterate through all the rows one by one
             for (Row row: sheet) {
-                // Temp var
                 List<Object> rowData = new ArrayList<>();
-                // Iterate over all the cells one by one
+
                 for(Cell cell: row) {
                     if(cell != null) {
                         switch (cell.getCellType()) {
-                            case STRING -> rowData.add(cell.getStringCellValue());
+                            case STRING ->  rowData.add(cell.getStringCellValue());
                             case NUMERIC -> rowData.add(cell.getNumericCellValue());
                             case BOOLEAN -> rowData.add(cell.getBooleanCellValue());
                         }
@@ -76,7 +72,7 @@ public class Activity10 {
 
     @DataProvider(name = "Registration")
     public static Object[][] signUpInfo() {
-        String filePath = "Activity10.xlsx";
+        String filePath = "C:/Users/08109U744/IdeaProjects/FST_TstNG/src/test/java/Activity10.xlsx";
         List<List<Object>> data = readExcel(filePath);
         return new Object[][] {
                 { data.get(1) },
@@ -92,36 +88,35 @@ public class Activity10 {
         WebElement emailField = driver.findElement(By.id("email"));
         WebElement phoneNumberField = driver.findElement(By.id("number"));
 
-        // Clear the fields
+
         firstNameField.clear();
         lastNameField.clear();
         emailField.clear();
         phoneNumberField.clear();
 
-        // Enter the data
+
         firstNameField.sendKeys(rows.get(1).toString());
         lastNameField.sendKeys(rows.get(2).toString());
         emailField.sendKeys(rows.get(3).toString());
         phoneNumberField.sendKeys(rows.get(4).toString());
 
-        // Click on the submit button
+
         driver.findElement(By.cssSelector("input.green")).click();
 
-        // Wait for the alert to show up
-        wait.until(ExpectedConditions.alertIsPresent());
 
-        // Switch to Alert
+       wait.until(ExpectedConditions.alertIsPresent());
+
+
         Alert message = driver.switchTo().alert();
-        // Get the alert message
         System.out.println("Alert Message: " + message.getText());
         Reporter.log("Alert Message: " + message.getText());
         message.accept();
 
-        // Refresh the page
+
         driver.navigate().refresh();
     }
 
-    @AfterClass
+    @AfterMethod
     public void teardown() {
         driver.quit();
     }
